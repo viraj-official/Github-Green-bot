971596816f48c38735d5382b6ff641de696563de
Viraj Shakya Samaranayake <virajofficialinfo@gmail.com>
2023-06-04 20:36:17 +0530

    Update git.py

--- git.py
+++ git.py
@@ -1,10 +1,10 @@
 import os , random
 
-for i in range(200):
+for i in range(120):
     d = str(i) + 'days ago'
-    rand = random.randrange(1, 12)
+    rand = random.randrange(1, 6)
     with open('test.txt','a') as file:
         file.write(d+'\n')
     os.system('git add test.txt')
-    os.system('git commit --date=" 2020-'+str(rand)+'-'+d+'" -m 1')
+    os.system('git commit --date=" 2023-'+str(rand)+'-'+d+'" -m 1')
 os.system('git push -u origin main')
